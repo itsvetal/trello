63244d384179c5548dcd373e8918a2adf81f76e4
iTsVetal <vitaliikryskiv.development@gmail.com>
2024-11-10 21:37:54 +0200

    Made the changing of text color based on background color V1.1.2

--- src/pages/Home/components/Board/Board.tsx
+++ src/pages/Home/components/Board/Board.tsx
@@ -6,20 +6,20 @@ export function Board({ title, custom }: IBoard): React.ReactElement {
   const values = Object.values(custom);
   const value = values[0];
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const hexToRgb = (hex: string): number[] => {
     const r = parseInt(hex.slice(1, 3), 16);
     const g = parseInt(hex.slice(3, 5), 16);
     const b = parseInt(hex.slice(5, 7), 16);
     return [r, g, b];
   };
-  // const [r, g, b] = hexToRgb(value);
+  const [r, g, b] = hexToRgb(value);
 
   return (
     <div
       className="home-board"
       style={{
         backgroundColor: value,
+        color: r >= 220 && g >= 220 && b >= 220 ? 'black' : `white`,
       }}
     >
       {title}
